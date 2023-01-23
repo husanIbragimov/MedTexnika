19b711919e27dc556f640d5e8a2e22e409f4067f
Husan Ibragimov <ibragmovhusan283@gmail.com>
2023-01-23 16:46:40 +0500

    created contact views.py

---
+++ apps/contact/api/views.py
@@ -0,0 +1,13 @@
+from apps.contact.models import GetInTouch, Subscribe
+from rest_framework import generics, permissions
+from .serializers import GetInTouchSerializer, SubscribeSerializer
+
+
+class GetInTouchCreateAPIView(generics.CreateAPIView):
+    queryset = GetInTouch.objects.all()
+    serializer_class = GetInTouchSerializer
+
+
+class SubscribeCreateAPIView(generics.CreateAPIView):
+    queryset = Subscribe.objects.filter(is_active=True).order_by('-id')
+    serializer_class = SubscribeSerializer
