48833f46d305d1b0d96d30280b80fb8407b46e8f
Husan Ibragimov <ibragmovhusan283@gmail.com>
2023-01-23 17:42:01 +0500

    created product admin.py

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Product, ProductImage, Brand, Banner, Category
+# from modeltranslation.admin import TranslationAdmin
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'parent_category', 'is_active', 'date_created')
+    list_filter = ('parent_category', 'is_active', 'date_created')
+    autocomplete_fields = ('parent_category',)
+    search_fields = ('title',)
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
+    list_display = ['id', 'name', 'price', 'get_discounted_price', 'status', 'brand', 'created_at']
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ['name', 'description']
+    filter_horizontal = ('category',)
+    list_filter = ('category', 'brand', 'made_in', 'created_at', 'is_active')
+
+
+admin.site.register(Brand)
+admin.site.register(Banner)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
