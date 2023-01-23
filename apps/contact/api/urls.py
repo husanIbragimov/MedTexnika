909afbd451e5df2a260d8c66e1aac35a3d7b12de
Husan Ibragimov <ibragmovhusan283@gmail.com>
2023-01-23 16:48:00 +0500

    created contact urls.py

--- apps/contact/api/urls.py
+++ apps/contact/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import GetInTouchCreateAPIView, SubscribeCreateAPIView
 
 urlpatterns = [
-
+    path('create/', GetInTouchCreateAPIView.as_view()),
+    path('subscribe/', SubscribeCreateAPIView.as_view())
 ]
