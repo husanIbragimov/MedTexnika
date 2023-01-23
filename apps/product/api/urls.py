3a798cb31ffac7209acc7d993d675f02a2cbdc54
Husan Ibragimov <ibragmovhusan283@gmail.com>
2023-01-23 18:12:46 +0500

    created product urls.py

--- apps/product/api/urls.py
+++ apps/product/api/urls.py
@@ -1,5 +1,22 @@
 from django.urls import path
 
-urlpatterns = [
+from apps.product.api.views import CategoryListAPIView, CategoryRetrieveAPIView, BrandListAPIView, BrandRetrieveAPIView, \
+    ProductCreateAPIView, ProductListAPIView, NewProductListAPIView, ProductRetrieveAPIView, BannerListAPIView
 
+urlpatterns = [
+    # category -->
+    path('3category/', CategoryListAPIView.as_view()),
+    path('3category/<int:pk>/', CategoryRetrieveAPIView.as_view()),
+
+    # brand
+    path('4brand-list/', BrandListAPIView.as_view()),
+    path('4brand/<int:pk>/', BrandRetrieveAPIView.as_view()),
+
+    # product
+    path('5product-create/', ProductCreateAPIView.as_view()),
+    path('5product/', ProductListAPIView.as_view()),
+    path('5product/new/', NewProductListAPIView.as_view()),
+    path('5product/<int:pk>/', ProductRetrieveAPIView.as_view()),
+
+    path('banner/', BannerListAPIView.as_view())
 ]
