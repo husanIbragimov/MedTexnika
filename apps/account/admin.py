a913af09e7eaadd69b2dddd9ccd17e5e990106cb
Husan Ibragimov <ibragmovhusan283@gmail.com>
2023-01-23 15:04:42 +0500

    created account's admin

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Account
 
-# Register your models here.
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'username', 'gender', 'is_active')
+    prepopulated_fields = {"slug": ("username",)}
+
+
+admin.site.register(Account, AccountAdmin)
