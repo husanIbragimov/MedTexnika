24c445129d0e0e488f9d90e1fb101369e6f590f3
Husan Ibragimov <ibragmovhusan283@gmail.com>
2023-01-23 16:04:54 +0500

    created blog model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=221)
+    slug = models.SlugField(unique=True, null=True)
+    image = models.ImageField(upload_to='articles/blog_image/')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse("article_detail", kwargs={"slug": self.slug})
+
+    def __str__(self):
+        return self.title
