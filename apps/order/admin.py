92cc3c9f144c79c27f1a14616996e299dea931f5
Husan Ibragimov <ibragmovhusan283@gmail.com>
2023-02-18 16:46:44 +0500

    filter user

--- apps/order/admin.py
+++ apps/order/admin.py
@@ -12,7 +12,7 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = [OrderItemsTabularInline]
     list_display = ('id', 'client', 'phone', 'get_quantity', 'get_summa', 'status', 'created_at')
     list_filter = ('status', 'created_at')
-    search_fields = ('client', 'phone', 'address', 'note')
+    search_fields = ('client__first_name', 'phone', 'address', 'note')
     readonly_fields = ('client', 'phone', 'address', 'zipcode', 'note', 'created_at',)
 
 
